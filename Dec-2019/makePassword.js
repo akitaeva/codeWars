230def4deac6be5699f3b25fd6d8cf662ea30d12
Alina Kitaeva <akitaeva@gmail.com>
2018-12-13 00:06:40 -0500

    daly codewars

--- Dec-2019/makePassword.js
+++ Dec-2019/makePassword.js
@@ -1,4 +1,22 @@
+//https://www.codewars.com/kata/5637b03c6be7e01d99000046/solutions/javascript
+
+
 const makePassword = (phrase) =>{
- 
- 
-}
+    const wordArr = phrase.split(" ").map(x => x.charAt(0));
+    const finArr = []
+    wordArr.forEach(x => {
+      switch ( x.toLowerCase() ) {
+        case 'i':
+        x = '1';
+        break;
+        case 'o':
+        x = '0';
+        break;
+        case 's':
+        x = '5';
+        }
+     finArr.push(x);   
+    })
+    console.log(wordArr, finArr);
+    return finArr.join('')
+   }
